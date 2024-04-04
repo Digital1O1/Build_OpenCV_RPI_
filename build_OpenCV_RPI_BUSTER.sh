a2da7daea21f6a7ac6a3b00660584d0ccfdd97ff
Christopher Ta <Christopher.Ta1125@gmail.com>
2024-04-04 11:24:44 -0500

    Update build_OpenCV_RPI_BUSTER.sh

--- build_OpenCV_RPI_BUSTER.sh
+++ build_OpenCV_RPI_BUSTER.sh
@@ -7,6 +7,9 @@ sudo apt-get install -y build-essential cmake git libgtk2.0-dev pkg-config \
                         libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev \
                         python3-dev python3-numpy
 
+PYTHON_VERSION=$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')
+
+
 # Clone OpenCV repository
 cd ~
 git clone https://github.com/opencv/opencv.git
@@ -29,7 +32,7 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
       -D BUILD_opencv_python2=NO \
       -D BUILD_opencv_python3=YES \
       -D PYTHON_DEFAULT_EXECUTABLE=/usr/bin/python3 \
-      -D OPENCV_PYTHON3_INSTALL_PATH=/usr/local/lib/python3.7/dist-packages \
+      -D OPENCV_PYTHON3_INSTALL_PATH=/usr/local/lib/python${PYTHON_VERSION}/dist-packages \
       -D OPENCV_ENABLE_NONFREE=YES \
       -D INSTALL_PYTHON_EXAMPLES=NO ..
 
